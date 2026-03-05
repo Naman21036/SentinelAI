2983058cad3919d5c7acf9f28c6dd43a69dfe255
Naman <namangupta2132@gmail.com>
2026-03-05 14:43:51 +0530

    Changes in requirements.txt

--- static/js/styles.js
+++ static/js/styles.js
@@ -1,1 +1,13 @@
 console.log("SentinelAI UI Loaded")
+
+document.addEventListener("DOMContentLoaded", () => {
+
+const btn = document.querySelector(".analyze-button")
+
+btn.addEventListener("click", () => {
+
+btn.innerHTML = "Analyzing..."
+
+})
+
+})
