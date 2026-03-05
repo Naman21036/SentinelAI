812d24d2e1bd4efcaa1a1c7589a3734efd1e814d
Naman <namangupta2132@gmail.com>
2026-03-05 20:02:46 +0530

    Added AI typing

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,19 @@
-const text = "AI powered hate speech detection system"
+const text = "AI powered hate speech detection system";
 
-let index = 0
+let index = 0;
 
 function typeEffect(){
 
 if(index < text.length){
 
-document.getElementById("ai-typing").innerHTML += text.charAt(index)
+document.getElementById("ai-typing").innerHTML += text.charAt(index);
 
-index++
+index++;
 
-setTimeout(typeEffect,35)
-
-}
+setTimeout(typeEffect,35);
 
 }
 
-typeEffect()
-
-const btn = document.getElementById("analyzeBtn")
-
-if(btn){
-
-btn.addEventListener("click",()=>{
-
-document.getElementById("spinner").classList.remove("hidden")
-
-})
-
 }
+
+window.onload = typeEffect;
