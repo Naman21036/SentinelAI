afd133a065d079307a4be675f5db3984c8e1f1f1
Naman <namangupta2132@gmail.com>
2026-03-13 00:42:18 +0530

    deploy error fixed

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routers import predict
+import os
+import uvicorn
 
 app = FastAPI(title="SentinelAI")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(predict.router)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
