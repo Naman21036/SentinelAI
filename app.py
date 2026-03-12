0c87baf4a6d55b268ee61a7f773806d70bc32a49
Naman <namangupta2132@gmail.com>
2026-03-12 23:21:50 +0530

    deploy fix1

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -10,4 +11,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(predict.router)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
